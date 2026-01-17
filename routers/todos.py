9f7e4d84041c0662d6b565524779542c08e43d36
Mahmoud <mahmoudnasser1561@gmail.com>
2026-01-17 23:19:16 +0200

    create todo

--- routers/todos.py
+++ routers/todos.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, Path
 from database import SessionLocal
 from starlette import status
+from pydantic import BaseModel, Field
 from models import Todos
 
 router = APIRouter()
@@ -18,6 +19,12 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
 @router.get("/")
 async def read_all(db: Annotated[Session, Depends(get_db)]):
     return db.query(Todos).all()
@@ -28,3 +35,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(session_code=404, detail="todo not found.")
+
+@router.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
