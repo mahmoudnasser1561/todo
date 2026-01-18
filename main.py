9e582082859bae20b39343f19ea0873ef08076c0
Mahmoud <mahmoudnasser1561@gmail.com>
2026-01-18 16:37:37 +0200

    added create user /auth

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import todos
+from routers import todos, auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(todos.router)
+app.include_router(auth.router)
