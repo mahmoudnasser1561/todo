a1a7dc5d91cfaaf1da0d361b5f06e4b8fae6cca6
Mahmoud <mahmoudnasser1561@gmail.com>
2026-01-19 13:24:59 +0200

    protected put endpoint

--- routers/todos.py
+++ routers/todos.py
@@ -54,8 +54,11 @@ async def create_todo(db: db_dependency, user: user_dependency,
     db.commit()
 
 @router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def update_todo(db: db_dependency, user:user_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = db.query(Todos).filter(Todos.id == todo_id)\
+        .filter(Todos.owner_id == user.get('id')).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail='Todo not found.')
 
